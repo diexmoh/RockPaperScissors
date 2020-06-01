9366d9604b10d97cc42a24b9824d56800e6c48bc
Diego Olvera <diego.olvera97@gmail.com>
2020-06-01 01:53:45 -0500

    First Commit

--- Rock Paper Scissors/ViewController.swift
+++ Rock Paper Scissors/ViewController.swift
@@ -10,11 +10,58 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var labelView: UILabel!
+    @IBOutlet weak var systemView: UIImageView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
-
-
+    
+    let arrayPlays = [#imageLiteral(resourceName: "rock"), #imageLiteral(resourceName: "paper"), #imageLiteral(resourceName: "scissors")]
+    
+    @IBAction func rockOption(_ sender: Any) {
+        systemView.image = arrayPlays.randomElement()
+        whoWins(playerChoice: 0)
+    }
+    
+    @IBAction func paperOption(_ sender: Any) {
+        systemView.image = arrayPlays.randomElement()
+        whoWins(playerChoice: 1)
+    }
+    
+    @IBAction func scissorsOption(_ sender: Any) {
+        systemView.image = arrayPlays.randomElement()
+        whoWins(playerChoice: 2)
+    }
+    func whoWins(playerChoice: Int){
+        if(playerChoice == 0){
+            if(systemView.image == #imageLiteral(resourceName: "rock")){
+                labelView.text = "Draw!"
+            }else if(systemView.image == #imageLiteral(resourceName: "scissors")){
+                labelView.text = "You win!"
+            }else if(systemView.image == #imageLiteral(resourceName: "paper")){
+                labelView.text = "You lose!"
+            }
+        }
+        if(playerChoice == 1){
+            if(systemView.image == #imageLiteral(resourceName: "paper")){
+                labelView.text = "Draw!"
+            }else if(systemView.image == #imageLiteral(resourceName: "rock")){
+                labelView.text = "You win!"
+            }else if(systemView.image == #imageLiteral(resourceName: "scissors")){
+                labelView.text = "You lose!"
+            }
+        }
+        if(playerChoice == 2){
+            if(systemView.image == #imageLiteral(resourceName: "scissors")){
+                labelView.text = "Draw!"
+            }else if(systemView.image == #imageLiteral(resourceName: "paper")){
+                labelView.text = "You win!"
+            }else if(systemView.image == #imageLiteral(resourceName: "rock")){
+                labelView.text = "You lose!"
+            }
+        }
+    }
 }
 
